70b4b0e35818d33787d5b4d310a2fe8f76eec49d
Hong JiHoo <gmacto123@gmail.com>
2019-09-29 23:25:29 +0900

    commit

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-var dockerCLI = require('docker-cli-js');
-var DockerOptions = dockerCLI.Options;
-var Docker = dockerCLI.Docker;
+'use strict';
+const {Docker} = require('node-docker-api');
 
-var docker = new Docker();
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
 
-docker.command('ps', function (err, data) {
-    console.log(data);
-});
+// List
+docker.container.list()
+   // Inspect
+  .then(containers => containers[0].status())
+  .then(container => container.stats())
+  .then(stats => {
+    stats.on('data', stat => console.log('Stats: ', stat.toString()))
+    stats.on('error', err => console.log('Error: ', err))
+  })
+  .catch(error => console.log(error));
