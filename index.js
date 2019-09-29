e4b25d7e09c81830d88598976c42f2190edcaa3a
Hong JiHoo <gmacto123@gmail.com>
2019-09-29 20:27:30 +0900

    TEST Command 2

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var Docker = dockerCLI.Docker;
 
 var docker = new Docker();
 
-docker.command('info', function (err, data) {
-    console.log('data = ', data);
+docker.command('ps', function (err, data) {
+    console.log(data);
 });
